80142da1d151a44a351d12a9df0cb920d955376f
Jose Tavares <josetavares00@gmail.com>
2020-01-15 20:59:14 +0000

    remove gatsby-plugin-feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,6 @@ module.exports = {
         `gatsby-plugin-emotion`,
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
-        `gatsby-plugin-feed`,
         `gatsby-plugin-offline`
     ]
 };
