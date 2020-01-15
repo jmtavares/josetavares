0aefb199803dcfedb407cc7f0b826d2357b71514
Jose Tavares <josetavares00@gmail.com>
2020-01-15 20:52:40 +0000

    remove unnecessary plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,13 +6,6 @@ module.exports = {
         siteUrl: `https://www.josetavares.me`
     },
     plugins: [
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: `${__dirname}/content/blog`,
-                name: `blog`
-            }
-        },
         `gatsby-plugin-sitemap`,
         `gatsby-plugin-react-helmet`,
         `gatsby-plugin-robots-txt`,
@@ -45,27 +38,6 @@ module.exports = {
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
         `gatsby-plugin-feed`,
-        {
-            resolve: `gatsby-transformer-remark`,
-            options: {
-                plugins: [
-                    {
-                        resolve: `gatsby-remark-images`,
-                        options: {
-                            maxWidth: 590
-                        }
-                    },
-                    {
-                        resolve: `gatsby-remark-responsive-iframe`,
-                        options: {
-                            wrapperStyle: `margin-bottom: 1.0725rem`
-                        }
-                    },
-                    `gatsby-remark-copy-linked-files`,
-                    `gatsby-remark-smartypants`
-                ]
-            }
-        },
         `gatsby-plugin-offline`
     ]
 };
